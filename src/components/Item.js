54a7d2233f403396afcd55890a7ccf0458719937
Breon White <bdwhite92@gmail.com>
2022-05-27 17:22:56 -0400

    commit tree ea9bf4a25faad0b19174b852959118da13b71528

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemInCart, updateCart] = useState(false);
+
+  function addItemToCart(){
+    updateCart((itemInCart) => !itemInCart);
+  }
+  
   return (
-    <li className="">
+    <li className={itemInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className={itemInCart ? "remove" : "add"}
+        onClick={addItemToCart}>
+        {itemInCart ? "Remove Item From" : "Add Item to"} Cart
+      </button>
     </li>
   );
 }
